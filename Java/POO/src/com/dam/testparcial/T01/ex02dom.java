01d61d102179591196f1601ccded5cf474cdc15b
Damaga <damagasnake@gmail.com>
2025-10-27 21:26:21 +0100

    modified for 5 loops

--- Java/POO/src/com/dam/testparcial/T01/ex02dom.java
+++ Java/POO/src/com/dam/testparcial/T01/ex02dom.java
@@ -3,19 +3,21 @@ import java.util.Scanner;
 public class ex02dom {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int i = 2;
-        int cont = 0;
-        System.out.println("introduce el primer num");
-        int n1 = Integer.parseInt(sc.nextLine());
-        while (i < n1) {
-            if (n1 % i == 0) {
-                cont++;
+
+        for (int vuelta = 1; vuelta <= 5; vuelta++) {
+            int i = 2;
+            int cont = 0;
+            System.out.println("Vuelta " + vuelta + " - introduce un num");
+            int n1 = Integer.parseInt(sc.nextLine());
+            while (i < n1) {
+                if (n1 % i == 0) {
+                    cont++;
+                }
+                i++;
             }
-            i++;
+            System.out.println(cont);
         }
-        System.out.println(cont);
-        cont = 0;
-        i = 2;
+
         sc.close();
     }
 }
